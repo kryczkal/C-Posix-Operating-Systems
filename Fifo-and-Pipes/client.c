eaf236d61824cbcb79dc6912c2f80e02d3876edf
Łukasz Kryczka <elek123456789101112@gmail.com>
2024-03-05 21:35:41 +0100

    Fifo-Pipe working server-client communication

--- Fifo-and-Pipes/client.c
+++ Fifo-and-Pipes/client.c
@@ -8,8 +8,10 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <memory.h>
 
 #define ERR(source) (perror(source), fprintf(stderr, "%s:%d\n", __FILE__, __LINE__), exit(EXIT_FAILURE))
+#define MSG_SIZE PIPE_BUF - sizeof(pid_t)
 void usage(char *name)
 {
     fprintf(stderr, "USAGE: %s fifo_file\n", name);
@@ -18,21 +20,29 @@ void usage(char *name)
 
 int read_file_send_to_fifo(char* fifo_name, char* file_name)
 {
+    // Messages need to be sent with the process pid at front;
     ssize_t count;
     char buffer[PIPE_BUF];
+    __pid_t pid = getpid();
+    *((pid_t*) buffer) = pid; // Set the pid at the front of the buffer
+    size_t offset = sizeof(pid_t);
+    char* msg_pointer = buffer + offset; // Set pointer to the end of the pid in buffer
+
     int fd = open(file_name, O_RDONLY);
     if(fd < 0) ERR("open");
 
     int fifo = open(fifo_name, O_WRONLY);
     if(fifo < 0) ERR("open");
 
-    while(read(fd, buffer, PIPE_BUF) > 0)
+    while((count = read(fd, msg_pointer, MSG_SIZE)) > 0)
     {
-        if((count = write(fifo, buffer, PIPE_BUF)) < 0) ERR("write");
+        if(count < MSG_SIZE) memset(msg_pointer + count, 0, MSG_SIZE - count);
+        if((write(fifo, buffer, PIPE_BUF)) < 0) ERR("write");
     }
 
     if(close(fd) < 0) ERR("close");
     if(close(fifo) < 0) ERR("close");
+    return EXIT_SUCCESS;
 }
 
 int main(int argc, char** argv)
@@ -44,4 +54,3 @@ int main(int argc, char** argv)
     read_file_send_to_fifo(fifo_name, file_name);
     return EXIT_SUCCESS;
 }
-
--- Fifo-and-Pipes/server.c
+++ Fifo-and-Pipes/server.c
@@ -10,6 +10,7 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <linux/limits.h>
 
 #define ERR(source) (perror(source), fprintf(stderr, "%s:%d\n", __FILE__, __LINE__), exit(EXIT_FAILURE))
 
@@ -22,13 +23,22 @@ void usage(char *name)
 void read_from_fifo(int fifo)
 {
     ssize_t count;
-    char c;
+    char buffer[PIPE_BUF];
+    pid_t sender_pid;
     do
     {
-        if ((count = read(fifo, &c, 1)) < 0)
+        if ((count = read(fifo, &buffer, PIPE_BUF)) < 0)
             ERR("read");
-        if (count > 0 && isalnum(c))
-            printf("%c", c);
+        if (count > 0) {
+            sender_pid = *((pid_t *) buffer);
+            printf("Sender pid: %d | Message: ", sender_pid);
+            for(int i = sizeof(pid_t); i < PIPE_BUF; i++)
+            {
+                if(isalnum(buffer[i]))
+                    printf("%c", buffer[i]);
+            }
+            printf("\n");
+        }
     } while (count > 0);
 }
 
